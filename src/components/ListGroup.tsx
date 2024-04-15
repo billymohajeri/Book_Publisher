261216a934d6b983f616c02ede86762a0f561f67
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-04-15 11:50:00 +0300

    Add list group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,13 @@
 function ListGroup() {
-  return <h1>List Group</h1>;
+  return (
+    <ul class="list-group">
+      <li class="list-group-item">An item</li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+      <li class="list-group-item">A fourth item</li>
+      <li class="list-group-item">And a fifth one</li>
+    </ul>
+  );
 }
 
 export default ListGroup;

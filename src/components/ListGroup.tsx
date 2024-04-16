b89b0483ff3136d16e6974108f226b2f29c38569
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-04-16 12:55:41 +0300

    Render li items dynamically

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,12 @@
 function ListGroup() {
+  const items = ["One", "Two", "Three", "Four", "Five"];
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        
+        {items.map((item) => (
+          <li className="list-group-item">{item}</li>
+        ))}
       </ul>
     </>
   );

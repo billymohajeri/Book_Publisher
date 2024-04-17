3e70ec5326a7bf3cd3509387df03acd26cc6649e
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-04-18 01:38:59 +0300

    Add onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,7 +7,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item} className="list-group-item">
+          <li key={item} className="list-group-item" onClick={()=>console.log(item,'Clicked!')}>
             {item}
           </li>
         ))}

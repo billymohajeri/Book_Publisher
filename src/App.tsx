ef6451e3f18f222ab7d9c4ef7d9625fc614a7c69
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-04-20 12:22:14 +0300

    Add function to props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["One", "Two", "Three", "Four", "Five"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup heading="Numbers" items={items} />
+      <ListGroup
+        heading="Numbers"
+        items={items}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }

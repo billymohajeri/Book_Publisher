d305177eb0f49f8604c18ac4138c56c9e3374ba3
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-04-23 13:30:33 +0300

    Change alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
   let items = ["One", "Two", "Three", "Four", "Five"];
   const handleSelectItem = (item: string) => {
     console.log(item);
   };
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-      <Button onClick={() => console.log("Button clicked!")} >
-        Click me!
-      </Button>
+      {alertVisible && (
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      )}
+      <Button onClick={() => console.log("Button clicked!")}>Click me!</Button>
       <ListGroup
         heading="Numbers"
         items={items}

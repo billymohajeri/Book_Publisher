778708c1bb3a68b3754c4c18525e454aeb120d72
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-05-13 15:14:23 +0300

    Add option for all cats

---
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExpenseFilter = () => {
+  return (
+    <select className="form-select">
+      <option value="">All Categories</option>
+    </select>
+  );
+};
+
+export default ExpenseFilter;

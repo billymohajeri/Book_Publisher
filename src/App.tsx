b2b5cf4450de51be2affef039c5b2074deaf7813
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-05-13 16:06:04 +0300

    Add state to app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "aaa", amount: 10, category: "Utilities" },
     { id: 2, description: "bbb", amount: 20, category: "Utilities" },

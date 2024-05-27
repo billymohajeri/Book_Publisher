7d7b13fe3d9320fb9f7a1f50ee4063c86cf31d7b
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-05-27 17:56:54 +0300

    Add description

--- src/expense-tracker/components/ExpenseForm.tsx
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const ExpenseForm = () => {
   return (
-    <form action="">
-        
+    <form>
+      <div className="mb-3">
+        <label htmlFor="description" className="form">
+          Description
+        </label>
+        <input id="description" type="text" className="form-control" />
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
